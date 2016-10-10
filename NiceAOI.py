eb0fbdad9344de0ebbb5b237579abef2b38ff606
Fboub <francois.boubila@lgm.fr>
2016-10-10 15:03:04 +0200

    Update NiceAOI.py

--- NiceAOI.py
+++ NiceAOI.py
@@ -6,9 +6,9 @@ api_key = os.environ['9A1B7AC1899140B2B50D']
 api_secret = os.environ['F40B598E6DBB44B29FD0D71401F3F0AB']
 url = 'https://api.urthecast.com/v1/consumers/apps/me/aois'
 
-r = requests.post(url,
+r = requests.post(https://api.urthecast.com/v1/consumers/apps/me/aois,
     headers = { 'Content-Type': 'application/json' },
-    params = { 'api_key': api_key, 'api_secret': api_secret },
+    params = { 'api_key': 9A1B7AC1899140B2B50D, 'api_secret': F40B598E6DBB44B29FD0D71401F3F0AB },
     # Check out http://geojson.io for help with creating GeoJSON objects
     data = json.dumps({
         "name": "San Francisco",
